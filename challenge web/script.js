a99728a9c2b92b4b1b862da6f6a4699361c3abd0
Shubham Pandey <55655416+shubhamtechnews@users.noreply.github.com>
2020-12-20 21:37:05 +0530

    Add files via upload

--- challenge web/script.js
+++ challenge web/script.js
@@ -1,11 +1,25 @@
+setInterval(clock, 500);
+
 function clock() {
   let times = new Date();
   let hrs = times.getHours();
   let mins = times.getMinutes();
   let sec = times.getSeconds();
-  console.log(sec);
-  return `${hrs} : ${mins} : ${sec}`;
-}
+  let en = "AM";
 
-document.querySelector(".time").textContent = clock();
-setInterval(document.querySelector(".time").textContent, 500);
+  if (hrs > 12) {
+    en = "PM";
+    hrs -= 12;
+  }
+  if (hrs < 10) {
+    hrs = `0${hrs}`;
+  }
+  if (mins < 10) {
+    mins = `0${mins}`;
+  }
+  if (sec < 10) {
+    sec = `0${sec}`;
+  }
+
+  document.querySelector(".time").textContent = `${hrs}:${mins}:${sec} ${en}`;
+}
