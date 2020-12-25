8029339d4c4bae606a49f1b54b6bc0c4ee4e78b8
Shubham Pandey <55655416+shubhamtechnews@users.noreply.github.com>
2020-12-25 23:08:53 +0530

    Add files via upload

--- weather/api.js
+++ weather/api.js
@@ -1,25 +1,87 @@
+'use strict';
+// ! -----------------------------------Dark Mode-----------------------------------
+// ! -------------------------------------------------------------------------------
+
 const darkMode = document.querySelector('.darkmode input');
 const switchDark = document.querySelector('.darkmode span');
 const info = document.querySelector('.info');
-// console.log(darkMode);
-/* darkMode.addEventListener('checked',function(){
-    switchDark.classList.remove('span-btn');
-}) */
 
 // * functions
-let switchDarkFunction = function(){
+let switchDarkFunction = function () {
     info.classList.toggle('info__dark')
 }
 
-darkMode.addEventListener('click',function(){
-    if(darkMode.checked){
+darkMode.addEventListener('click', function () {
+    if (darkMode.checked) {
         switchDark.classList.toggle('span-btn-active');
         switchDarkFunction();
-    }
-    else{
+    } else {
         switchDark.classList.toggle('span-btn-active');
         switchDarkFunction();
     }
 })
 
+// ! -----------------------------------Quote Changer-----------------------------------
+// ! -----------------------------------------------------------------------------------
 
+
+const quoteBtn = document.querySelector('.quote-btn');
+let rno = 1;
+const quoteGenerateFunction = function () {
+    document.querySelector(`.quote-${rno}`).style.display = 'none';
+    rno = Number(Math.trunc((Math.random() * 4) + 1));
+    const quote = document.querySelector(`.quote-${rno}`);
+    quote.style.display = 'block';
+}
+
+document.querySelector('body').onload = quoteGenerateFunction;
+quoteBtn.addEventListener('click', function () {
+    quoteGenerateFunction();
+    quoteBtn.load
+});
+
+// ! -----------------------------------Date Reciever-----------------------------------
+// ! -----------------------------------------------------------------------------------
+
+setInterval(timer, 1000);
+
+function timer() {
+    let times = new Date();
+    let hrs = times.getHours();
+    let mins = times.getMinutes();
+    let en = "AM";
+
+    if (hrs > 12) {
+        en = "PM";
+        hrs -= 12;
+    }
+    if (hrs < 10) {
+        hrs = `0${hrs}`;
+    }
+    if (mins < 10) {
+        mins = `0${mins}`;
+    }
+    document.querySelector(".weather__time h1").textContent = `${hrs}:${mins} ${en}`;
+}
+
+function backgroundTimer() {
+    const backgImg = document.querySelector('main');
+    const weatherText = document.querySelector('.weather__desc span');
+    let times = new Date();
+    let hrs = times.getHours();
+    console.log(hrs);
+
+    if(hrs >= 0){
+        backgImg.style.background = `url('1.jpg')`;
+        weatherText.textContent = `GOOD MORNING, IT'S CURRENTLY`;
+    }
+    if(hrs >= 15 && hrs <= 19){
+        backgImg.style.background = `url('2.jpg')`;
+        weatherText.textContent = `GOOD EVENING, IT'S CURRENTLY`;
+    }
+    else{
+        backgImg.style.background = `url('3.jpg')`;
+        weatherText.textContent = `GOOD NIGHT, IT'S CURRENTLY`;
+    }
+}
+backgroundTimer();
